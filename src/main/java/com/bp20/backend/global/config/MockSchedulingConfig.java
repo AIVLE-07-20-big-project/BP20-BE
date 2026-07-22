c6825e142511b007b227be8ec3ac51ecc7dd60ff
Parkhuisang <huisang1030@naver.com>
2026-07-22 13:32:29 +0900

    refactor: consolidate configuration packages

--- src/main/java/com/bp20/backend/global/config/MockSchedulingConfig.java
+++ src/main/java/com/bp20/backend/global/config/MockSchedulingConfig.java
@@ -1,4 +1,4 @@
-package com.bp20.backend.config;
+package com.bp20.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
